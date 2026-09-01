3af9a331b9053b17fcdddf6bf410cc167ea4e324
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-01 12:54:37 +0900

    회원 가입 기능 테스트까지 구현

---
+++ backend/src/test/java/com/back/p67260811/domain/member/ApiV1MemberControllerTest.java
@@ -0,0 +1,77 @@
+package com.back.p67260811.domain.member;
+
+import com.back.p67260811.domain.member.controller.ApiV1MemberController;
+import com.back.p67260811.domain.member.entity.Member;
+import com.back.p67260811.domain.member.repository.MemberRepository;
+import com.back.p67260811.domain.member.service.MemberService;
+import com.back.p67260811.global.dto.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+public class ApiV1MemberControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원 가입")
+    void t1() throws Exception {
+
+        String username = "newUser";
+        String password = "1234";
+        String nickname = "새유저";
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/members")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "username": "%s",
+                                            "password": "%s",
+                                            "nickname": "%s"
+                                        }
+                                        """.formatted(username, password, nickname)
+                                )
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("join"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("회원가입이 완료되었습니다. %s님 환영합니다.".formatted(nickname)))
+                .andExpect(jsonPath("$.data.memberDto.id").value(6))
+                .andExpect(jsonPath("$.data.memberDto.createDate").exists())
+                .andExpect(jsonPath("$.data.memberDto.modifyDate").exists())
+                .andExpect(jsonPath("$.data.memberDto.name").value(username));
+    }
+}
