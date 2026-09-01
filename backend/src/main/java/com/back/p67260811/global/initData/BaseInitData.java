8790aa969fc9ec1eb037a95c3da2f2405ca1e786
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-01 09:47:15 +0900

    member domain

--- backend/src/main/java/com/back/p67260811/global/initData/BaseInitData.java
+++ backend/src/main/java/com/back/p67260811/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.p67260811.global.initData;
 
+import com.back.p67260811.domain.member.service.MemberService;
 import com.back.p67260811.domain.post.post.entity.Post;
 import com.back.p67260811.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +19,32 @@ public class BaseInitData {
     @Lazy
     private BaseInitData self;
     private final PostService postService;
+    private final MemberService memberService;
+
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
             self.work1();
+            self.work2();
         };
     }
 
     @Transactional
     public void work1() {
+        if(memberService.count() > 0) {
+            return;
+        }
+
+        memberService.join("system", "system", "시스템");
+        memberService.join("admin", "admin", "운영자");
+        memberService.join("user1", "1234", "유저1");
+        memberService.join("user2", "1234", "유저2");
+        memberService.join("user3", "1234", "유저3");
+
+    }
+
+    @Transactional
+    public void work2() {
         if (postService.count() > 0) {
             return;
         }
