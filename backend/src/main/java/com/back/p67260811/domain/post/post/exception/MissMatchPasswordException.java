20fcf567b0341f7d2ed6a4cfc3035385ef6b7108
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-01 11:40:44 +0900

    add ServiceException

---
+++ backend/src/main/java/com/back/p67260811/domain/post/post/exception/MissMatchPasswordException.java
@@ -0,0 +1,8 @@
+package com.back.p67260811.domain.post.post.exception;
+
+public class MissMatchPasswordException extends RuntimeException {
+
+    public MissMatchPasswordException(String message) {
+        super(message);
+    }
+}
