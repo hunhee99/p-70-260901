09e460bc3beb0b88351e9304ad2383d843dff562
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-02 09:25:42 +0900

    회원 가입 시 username 중복 방지 && 예외처리

--- backend/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
+++ backend/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
@@ -4,6 +4,7 @@ import com.back.p67260811.domain.member.dto.MemberDto;
 import com.back.p67260811.domain.member.entity.Member;
 import com.back.p67260811.domain.member.service.MemberService;
 import com.back.p67260811.global.dto.RsData;
+import com.back.p67260811.global.exception.ServiceException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
