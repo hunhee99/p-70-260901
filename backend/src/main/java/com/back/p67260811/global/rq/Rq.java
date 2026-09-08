5e390e3b6088a0ea351f1099280724875673b961
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-09-08 12:38:50 +0900

    CustomAuthenticationFilter에 인증 로직 작성

--- backend/src/main/java/com/back/p67260811/global/rq/Rq.java
+++ backend/src/main/java/com/back/p67260811/global/rq/Rq.java
@@ -88,11 +88,11 @@ public class Rq {
 
     }
 
-    private void setHeader(String name, String value) {
+    public void setHeader(String name, String value) {
         response.setHeader(name, value);
     }
 
-    private String getCookieValue(String name, String defaultValue) {
+    public String getCookieValue(String name, String defaultValue) {
         return Optional
                 .ofNullable(request.getCookies())
                 .flatMap(
@@ -127,7 +127,7 @@ public class Rq {
 
     }
 
-    private String getHeader(String name, String defaultValue) {
+    public String getHeader(String name, String defaultValue) {
         return Optional
                 .ofNullable(request.getHeader(name))
                 .filter(headerValue -> !headerValue.isBlank())
